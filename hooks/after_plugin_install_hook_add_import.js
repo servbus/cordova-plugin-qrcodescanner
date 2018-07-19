68b7de5a37eb296c2254ea51079e47f8d8e87c10
任跃兵 <cnryb@live.cn>
2018-07-19 11:21:29 +0800

    project name 和 package name 不同

--- hooks/after_plugin_install_hook_add_import.js
+++ hooks/after_plugin_install_hook_add_import.js
@@ -1,15 +1,24 @@
 var fs = require('fs');
 var path = require('path');
 
-var packageobj = JSON.parse(fs.readFileSync("package.json", 'utf8'));
-var packageName = packageobj.name;
-var path = `platforms/ios/${packageName}/Bridging-Header.h`;
 
-if (fs.existsSync(path)) {
+var configXml = fs.readFileSync("config.xml", 'utf8');
+configXml = configXml.toLowerCase();
+var reg = /<name>(.+)<\/name>/gi;
+
+var result;
+if ((result = reg.exec(configXml)) != null) {
+    var packageName = result[1];
     console.log("With the package name: " + packageName);
-    var data = fs.readFileSync(path, 'utf8');
-    var result = data + "\r\n#import <ZXingObjC/ZXingObjC.h>";
-    fs.writeFileSync(path, result, 'utf8');
-} else {
-    console.error(`Can not find Bridging-Header.h at path: ${path}`);
+    var path = `platforms/ios/${packageName}/Bridging-Header.h`;
+    
+    if (fs.existsSync(path)) {
+        var data = fs.readFileSync(path, 'utf8');
+        var result = data + "\r\n#import <ZXingObjC/ZXingObjC.h>";
+        fs.writeFileSync(path, result, 'utf8');
+    } else {
+        console.error(`Can not find Bridging-Header.h at path: ${path}`);
+    }
+}else{
+    console.error(`config.xml name node error `);
 }
