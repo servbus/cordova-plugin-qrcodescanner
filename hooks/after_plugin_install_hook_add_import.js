22ddca59615579d7e6e1a6b526975e0ee8724343
任跃兵 <cnryb@live.cn>
2018-07-19 15:06:07 +0800

    去掉新特性，老版本node不支持

--- hooks/after_plugin_install_hook_add_import.js
+++ hooks/after_plugin_install_hook_add_import.js
@@ -10,15 +10,15 @@ var result;
 if ((result = reg.exec(configXml)) != null) {
     var packageName = result[1];
     console.log("With the package name: " + packageName);
-    var path = `platforms/ios/${packageName}/Bridging-Header.h`;
+    var path = "platforms/ios/" + packageName + "/Bridging-Header.h";
     
     if (fs.existsSync(path)) {
         var data = fs.readFileSync(path, 'utf8');
         var result = data + "\r\n#import <ZXingObjC/ZXingObjC.h>";
         fs.writeFileSync(path, result, 'utf8');
     } else {
-        console.error(`Can not find Bridging-Header.h at path: ${path}`);
+        console.error("Can not find Bridging-Header.h at path: " + path);
     }
 }else{
-    console.error(`config.xml name node error `);
+    console.error("config.xml name node error ");
 }
